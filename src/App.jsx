7b8b78d14dd12e28838ac863a5a807b9471f7e8e
meherab-mhk <meherabh43@gmail.com>
2024-10-22 22:59:56 +0600

    banner video modify

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,10 @@ function App() {
             muted
             loop
           >
-            <source src="../images/bg-video.mp4" type="video/mp4" />
+            <source
+              src="https://s3-figma-videos-production-sig.figma.com/video/907900026577014002/TEAM/2e15/b653/-bba7-4454-a5cc-3ca982ed3987?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TVw9soNo9XQ0-SkoJE~7OyIzqkvFT~bzz6lrnIq38U-IEDo41GJ54p4tZj21MXytDQcrRGJGcB18VXlQMNo-COasLzjVCntETBXo03QuPbPkh7SJ-x~mb5~Ydk~E6-evOKQ67GdxpawwJB8h3F9d5Qzob6FwpWe1TFFClczTbYuUJcSp3ShuMSC2xaSx4izKEX8MCrUC6VXVrh85-wU5ADve6Ctdgin6Kq8AbXPs3NeGObWmoRFPyWfRRUdudwZrrvQal1zKhseUg9dPKpvIqE61GW5sf7l1CW497OrStTl6ogoWuGoF0YMc9zV0-MOdbThyWCo4YmosRl7Omhvu9A__"
+              type="video/mp4"
+            />
           </video>
           <Nav></Nav>
           <Hero></Hero>
