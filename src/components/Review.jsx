f69125de29937d108dfcc736457a26a23b0e0a6a
meherab-mhk <meherabh43@gmail.com>
2024-10-23 00:38:24 +0600

    review slider modify

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import "./Review.css";
 const Review = () => {
   var settings = {
     dots: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2000,
     infinite: true,
     speed: 500,
